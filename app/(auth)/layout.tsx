aafe2faf8be55b40f30b88f5e48832678b55afb8
Davide Filosa <davideclaypool@gmail.com!>
2024-02-21 08:05:09 +0100

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex items-center justify-center h-full bg-purple-100">
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
